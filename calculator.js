324de8c0d76722de22e8be8425ae7d8917c22870
asadkhan-cc <asad.khan@cooperativecomputing.com>
2022-08-07 07:22:04 -0700

    calculator is taking complete string with aurthmatic operators

--- calculator.js
+++ calculator.js
@@ -1,4 +1,24 @@
-const pusher = () => {
-    // document.getElementsByClassName(classout)
-    console.log("clicked");
+const pusher = (inp) => {
+    // document.getElementsById("output").innerHTML= "A";
+    // if (inp === "/" || inp === "*" || inp === "-" || inp === "+"  ) {
+    //     console.log("object");
+    //     equal()
+    // }
+     const asad = document.getElementById("output").innerText;
+     console.log(asad);
+     const res = asad+inp 
+     document.getElementById("output").innerHTML = res;
+}
+var cleaner= ()=>{
+    const asad = document.getElementById("output").innerText;
+    console.log(asad);
+    document.getElementById("output").innerHTML = "";
+    
+}
+var equal = () => {
+    const asad = document.getElementById("output").innerText;
+    console.log(asad);
+    var res = eval(asad);
+    document.getElementById("output").innerHTML = res;
+    // cleaner();
 }
